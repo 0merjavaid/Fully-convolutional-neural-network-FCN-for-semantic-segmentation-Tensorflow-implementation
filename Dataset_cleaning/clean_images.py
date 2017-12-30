69ee6b85fbe3879dfc2912713fa75f82b13d9d4c
0merjavaid <14besemjavaid@seecs.edu.pk>
2017-12-30 06:20:18 +0500

    cleaned data

--- Dataset_cleaning/clean_images.py
+++ Dataset_cleaning/clean_images.py
@@ -3,17 +3,43 @@ import cv2
 import numpy as np
 import os
 import glob
-if not os.path.exists("Dataset"):
-    os.makedirs("Dataset")
-os.chdir("../../IMAGES/Labels")
-"""for file in glob.glob("*.*"):
-    image=Image.open(file)
-    name="../../Retina-segmentation-with-FCN/"+"Dataset/"+file.split(".")[0]+".png"
-    print name
-    image.save(name)"""
+from skimage import data, exposure
+def clean():
+    if not os.path.exists("Dataset"):
 
-for file in glob.glob("*.*"):
-    image=Image.open(file)
-    name="../../Retina-segmentation-with-FCN/"+"Labels/"+file.split(".")[0]+".png"
-    print name
-    image.save(name)
+        os.makedirs("Dataset")
+    os.chdir("../../IMAGES/Labels")
+    """for file in glob.glob("*.*"):
+        image=Image.open(file)
+        name="../../Retina-segmentation-with-FCN/"+"Dataset/"+file.split(".")[0]+".png"
+        print name
+        image.save(name)"""
+
+    for file in glob.glob("*.*"):
+        image=Image.open(file)
+        name="../../Retina-segmentation-with-FCN/"+"Labels/"+file.split(".")[0]+".png"
+        print name
+        image.save(name)
+
+def enhance(im):
+    copy=im.copy()
+    B=im[:,:,0]
+    G=im[:,:,1]
+    R=im[:,:,2]
+    eqB = exposure.equalize_adapthist(B, clip_limit=0.0051)*255
+    eqG = exposure.equalize_adapthist(G, clip_limit=0.01)*255
+
+    eqR = exposure.equalize_adapthist(R, clip_limit=0.0051)*255
+    copy[:,:,0]=eqB
+    copy[:,:,1]=eqG
+    copy[:,:,2]=eqR
+    return copy.astype("uint8")
+def contrast(path):
+    os.chdir(path)
+    for file in glob.glob("*.png"):
+        im=cv2.imread(file)
+        enhanced=enhance(im)
+        name="Enhanced/"+file
+        cv2.imwrite(name,enhanced)
+
+contrast("/home/omer/Desktop/omer/retina/Retina-segmentation-with-FCN/Dataset/")
